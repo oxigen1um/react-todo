d9e8f05dd48fd47f6a99518e8c52491f6c837e6f
OXIGEN1UM <chembrid9e@gmail.com>
2018-04-06 18:17:54 +0300

    props validation

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+  import React from 'react';
+  import PropTypes from 'prop-types';
+  
+  function App(props) {
+    console.log(props);
+    return (
+      <main>
+        <header>
+          <h1>{props.title}</h1>
+        </header>
+        
+        <section className="todo-list">
+          <div className="todo completed">
+            <button className="checkbox icon">
+              <i className="material-icons">check_box</i>
+            </button>
+            
+            <span className="todo-title">Изучить JavaScript</span>
+            
+              <button className="delete icon">
+                <i className="material-icons">delete</i>
+              </button>  
+          </div>
+          
+          <div className="todo">
+            <button className="checkbox icon">
+              <i className="material-icons">check_box_outline_blank</i>
+            </button>
+            
+            <span className="todo-title">Изучить React</span>
+            
+              <button className="delete icon">
+                <i className="material-icons">delete</i>
+              </button>  
+          </div>
+        </section>
+      </main>
+    );
+  }
+  
+  App.propTypes = {
+    title: PropTypes.string
+  };
+  
+  App.defaultProps = {
+    title: 'React Todo'
+  };
+  
+  export default App;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-const title = React.createElement('h1', null, 'React Todo');
-const subtitle = React.createElement('p', { className: 'subtitle' }, 'Это моё первое приложение на React');
-const container = React.createElement('div', null, title, subtitle);
-
-const app = (
-  <div>
-      <h1>React Todo</h1>
-      <p className="subtitle">Manage your tasks with React</p>
-  </div>
-);
-
-ReactDOM.render(app, document.getElementById('root'))
-
+  import React from 'react';
+  import ReactDOM from 'react-dom';
+  
+  import App from './App';
+  
+  ReactDOM.render(<App />, document.getElementById('root'));
